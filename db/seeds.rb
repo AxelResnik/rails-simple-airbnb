37c022eed4fa318127cc96e4adfdf3ef47b20f89
Axel Resnik <axelresnik@gmail.com>
2019-06-18 16:20:07 +0300

    logic done, poor style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+10.times do
+  flat = [name: Faker::Music::RockBand.name,
+          address: Faker::Address.street_address,
+          description: Faker::Music.genre,
+          price_per_night: rand(1..100),
+          number_of_guests: rand(1..7)]
+  Flat.create!(flat)
+end
+puts 'Finished!'
